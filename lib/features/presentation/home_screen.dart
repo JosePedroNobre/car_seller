810d8f7079235442c95e4be3e43d8a17f28fa20c
José Nobre <41454761+JosePedroNobre@users.noreply.github.com>
2021-07-23 12:53:08 +0200

    added list car images

--- lib/features/presentation/home_screen.dart
+++ lib/features/presentation/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:car_seller_app/features/presentation/bloc/home_screen_bloc.dart';
+import 'package:car_seller_app/network/model/car_image.dart';
 import 'package:car_seller_app/network/responses/car_response.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
@@ -36,11 +37,11 @@ class _HomeScreenState extends State<HomeScreen> {
             current is HomeScreenError || current is HomeScreenLoading || current is HomeScreenLoaded,
         builder: (context, state) {
           if (state is HomeScreenLoading) {
-            return Center(child: CircularProgressIndicator());
+            _buildLoading();
           }
 
           if (state is HomeScreenLoaded) {
-            return _buildCarList(state.carResponse);
+            return _buildCarBody(state.carResponse);
           }
 
           if (state is HomeScreenError) {
@@ -51,11 +52,37 @@ class _HomeScreenState extends State<HomeScreen> {
         });
   }
 
-  _buildCarList(CarResponse? carResponse) {
-    return Text(carResponse?.price?.gross ?? "N/A");
+  _buildCarBody(CarResponse? carResponse) {
+    return carResponse != null
+        ? Column(
+            children: [
+              Text(carResponse.title ?? ""),
+              Expanded(
+                child: ListView.builder(
+                  shrinkWrap: false,
+                  itemCount: carResponse.images?.length ?? 0,
+                  itemBuilder: (context, index) {
+                    final item = carResponse.images?[index];
+                    return _buildCarItem(item);
+                  },
+                ),
+              ),
+            ],
+          )
+        : _buildEmptyState();
   }
 
-  _buildEmptyState() {}
+  _buildCarItem(CarImage? carImage) {
+    return Card(child: Image.network(carImage?.uri ?? ""));
+  }
+
+  _buildLoading() {
+    return Center(child: CircularProgressIndicator());
+  }
+
+  _buildEmptyState() {
+    return Center(child: Text("No car to load"));
+  }
 
   _buildErrorScreen() {
     return Center(child: Text("Error while Loading"));
